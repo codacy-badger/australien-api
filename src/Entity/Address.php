4a900e3fbafb445880483bd94b4d923ea8ef9622
Alexandre-T <alexandre.tranchant@gmail.com>
2018-08-07 18:22:57 +0200

    Getter and setter added and tested.

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -58,4 +58,114 @@ class Address
      * @var Locality
      */
     private $locality;
+
+    /**
+     * Id getter.
+     *
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Region getter.
+     *
+     * @return string
+     */
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    /**
+     * Post office box number getter.
+     *
+     * @return string
+     */
+    public function getPostOfficeBoxNumber(): ?string
+    {
+        return $this->postOfficeBoxNumber;
+    }
+
+    /**
+     * Postal code getter.
+     *
+     * @return string
+     */
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Street address getter.
+     *
+     * @return string
+     */
+    public function getStreetAddress(): ?string
+    {
+        return $this->streetAddress;
+    }
+
+    /**
+     * Locality address.
+     *
+     * @return Locality
+     */
+    public function getLocality(): ?Locality
+    {
+        return $this->locality;
+    }
+
+    /**
+     * Region setter.
+     *
+     * @param string $region
+     */
+    public function setRegion(string $region): void
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * Post office box number setter.
+     *
+     * @param string $postOfficeBoxNumber
+     */
+    public function setPostOfficeBoxNumber(string $postOfficeBoxNumber): void
+    {
+        $this->postOfficeBoxNumber = $postOfficeBoxNumber;
+    }
+
+    /**
+     * Postal code setter.
+     *
+     * @param string $postalCode
+     */
+    public function setPostalCode(string $postalCode): void
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * Street address setter.
+     *
+     * @param string $streetAddress
+     */
+    public function setStreetAddress(string $streetAddress): void
+    {
+        $this->streetAddress = $streetAddress;
+    }
+
+    /**
+     * Locality setter.
+     *
+     * @param Locality $locality
+     */
+    public function setLocality(Locality $locality): void
+    {
+        $this->locality = $locality;
+    }
 }
---
+++ tests/Entity/AddressTest.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * This file is part of the Berger Australien Application.
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Entity;
+
+use App\Entity\Address;
+use App\Entity\Locality;
+use PHPUnit\Framework\TestCase;
+
+class AddressTest extends TestCase
+{
+    /**
+     * Entity to test.
+     *
+     * @var Address
+     */
+    private $address;
+
+    /**
+     * Set up the test case.
+     */
+    public function setUp(): void
+    {
+        $this->address = new Address();
+    }
+
+    /**
+     * Test initialization.
+     */
+    public function testConstructor()
+    {
+        self::assertNull($this->address->getId());
+        self::assertNull($this->address->getLocality());
+        self::assertNull($this->address->getPostalCode());
+        self::assertNull($this->address->getPostOfficeBoxNumber());
+        self::assertNull($this->address->getRegion());
+        self::assertNull($this->address->getStreetAddress());
+    }
+
+    /**
+     * Test postal code setter and getter.
+     */
+    public function testPostalCode()
+    {
+        $postalCode = '33680';
+
+        $this->address->setPostalCode($postalCode);
+        self::assertEquals($postalCode, $this->address->getPostalCode());
+    }
+
+    /**
+     * Test locality setter and getter.
+     */
+    public function testLocality()
+    {
+        $locality = new Locality();
+
+        $this->address->setLocality($locality);
+        self::assertEquals($locality, $this->address->getLocality());
+    }
+
+    /**
+     * Test region setter and getter.
+     */
+    public function testRegion()
+    {
+        $region = 'Nouvelle Aquitaine';
+
+        $this->address->setRegion($region);
+        self::assertEquals($region, $this->address->getRegion());
+    }
+
+    /**
+     * Test post office box number setter and getter.
+     */
+    public function testPostOfficeBoxNumber()
+    {
+        $postOfficeBoxNumber = 'CS0001';
+
+        $this->address->setPostOfficeBoxNumber($postOfficeBoxNumber);
+        self::assertEquals($postOfficeBoxNumber, $this->address->getPostOfficeBoxNumber());
+    }
+
+    /**
+     * Test street address setter and getter.
+     */
+    public function testStreetAddress()
+    {
+        $streetAddress = '33680';
+
+        $this->address->setStreetAddress($streetAddress);
+        self::assertEquals($streetAddress, $this->address->getStreetAddress());
+    }
+}
