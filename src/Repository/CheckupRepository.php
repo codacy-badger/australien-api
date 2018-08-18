df9e21d6300bcb7df31a6a36d6e1057d25013dba
Alexandre-T <alexandre.tranchant@gmail.com>
2018-08-18 12:04:49 +0200

    Api test added.

---
+++ src/Repository/CheckupRepository.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Berger Australian Application.
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Checkup Repository Class.
+ */
+class CheckupRepository extends EntityRepository
+{
+}
--- tests/Api/HealthApiTest.php
+++ tests/Api/HealthApiTest.php
@@ -14,77 +14,73 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Class to test API on locality.
+ * Class to test API on health.
  */
-class LocalityApiTest extends WebTestCase
+class HealthApiTest extends WebTestCase
 {
     /**
-     * Get all localities.
+     * Get all healths.
      */
     public function testGetCollection()
     {
         $client = static::createClient();
-        $client->request('GET', '/api/localities.jsonld');
+        $client->request('GET', '/api/healths.jsonld');
         self::assertTrue($client->getResponse()->isSuccessful());
         self::assertJson($client->getResponse()->getContent());
 
         $jsonResponse = json_decode($client->getResponse()->getContent());
-        self::assertEquals('/api/contexts/Locality', $jsonResponse->{'@context'});
-        self::assertEquals('/api/localities', $jsonResponse->{'@id'});
-        self::assertEquals('https://schema.org/addressLocality', $jsonResponse->{'hydra:member'}[0]->{'@type'});
+        self::assertEquals('/api/contexts/Health', $jsonResponse->{'@context'});
+        self::assertEquals('/api/healths', $jsonResponse->{'@id'});
+        self::assertEquals('https://schema.org/Thing', $jsonResponse->{'hydra:member'}[0]->{'@type'});
     }
 
     /**
-     * Get all localities.
+     * Get all healths.
      */
     public function testGetItem()
     {
         $client = static::createClient();
-        $client->request('GET', '/api/localities/1.jsonld');
+        $client->request('GET', '/api/healths/1.jsonld');
         self::assertTrue($client->getResponse()->isSuccessful());
         self::assertJson($client->getResponse()->getContent());
 
         $jsonResponse = json_decode($client->getResponse()->getContent());
-        self::assertEquals('/api/contexts/Locality', $jsonResponse->{'@context'});
-        self::assertEquals('/api/localities/1', $jsonResponse->{'@id'});
-        self::assertEquals('https://schema.org/addressLocality', $jsonResponse->{'@type'});
-        self::assertEquals('1', $jsonResponse->localityId);
-        self::assertNotEmpty($jsonResponse->name);
-        self::assertNotNull($jsonResponse->geometry);
-        self::assertNotNull($jsonResponse->geometry->latitude);
-        self::assertNotNull($jsonResponse->geometry->longitude);
-        self::assertEquals('Point', $jsonResponse->geometry->type);
+        self::assertEquals('/api/contexts/Health', $jsonResponse->{'@context'});
+        self::assertEquals('/api/healths/1', $jsonResponse->{'@id'});
+        self::assertEquals('https://schema.org/Thing', $jsonResponse->{'@type'});
+        self::assertEquals('1', $jsonResponse->healthId);
+        self::assertEquals('MDR1', $jsonResponse->identifier);
     }
 
     /**
-     * Delete a locality is not possible.
+     * Delete a health is not possible.
      */
     public function testDeleteItem()
     {
         $client = static::createClient();
-        $client->request('DELETE', '/api/localities/1');
+        $client->request('DELETE', '/api/healths/1');
         self::assertFalse($client->getResponse()->isSuccessful());
         self::assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $client->getResponse()->getStatusCode());
     }
 
     /**
-     * Update a locality is not possible.
+     * Update a health is not possible.
      */
     public function testUpdateItem()
     {
         $client = static::createClient();
-        $client->request('PUT', '/api/localities/1');
+        $client->request('PUT', '/api/healths/1');
         self::assertFalse($client->getResponse()->isSuccessful());
         self::assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $client->getResponse()->getStatusCode());
     }
 
     /**
-     * Create a locality is not possible.
+     * Create a health is not possible.
      */
     public function testCreateItem()
     {
         $client = static::createClient();
-        $client->request('POST', '/api/localities/FR');
+        $client->request('POST', '/api/healths/1');
         self::assertFalse($client->getResponse()->isSuccessful());
         self::assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $client->getResponse()->getStatusCode());
     }
